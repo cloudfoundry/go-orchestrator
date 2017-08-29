e6ee69022447cb4f6bef554d4b5467df1399cba4
Andrew Poydence <apoydence@gmail.com>
2017-08-29 05:21:10 -0600

    Flatten tests

--- orchestrator_test.go
+++ orchestrator_test.go
@@ -31,98 +31,90 @@ func TestOrchestrator(t *testing.T) {
 		}
 	})
 
-	o.Group("with 3 worker nodes", func() {
+	o.Group("with 3 worker nodes and 3 tasks", func() {
 		o.BeforeEach(func(t TO) TO {
 			for i := 0; i < 3; i++ {
 				t.o.AddWorker(fmt.Sprintf("worker-%d", i))
+				t.o.AddTask(fmt.Sprintf("task-%d", i))
 			}
 			return t
 		})
 
-		o.Group("with 3 tasks", func() {
-			o.BeforeEach(func(t TO) TO {
-				for i := 0; i < 3; i++ {
-					t.o.AddTask(fmt.Sprintf("task-%d", i))
+		o.Group("no tasks yet assigned", func() {
+			o.Spec("it evenly splits tasks among the cluster", func(t TO) {
+				t.o.NextTerm(context.TODO())
+
+				Expect(t, t.spy.added).To(HaveLen(3))
+
+				Expect(t, t.spy.added["worker-0"]).To(HaveLen(1))
+				Expect(t, t.spy.added["worker-1"]).To(HaveLen(1))
+				Expect(t, t.spy.added["worker-2"]).To(HaveLen(1))
+
+				var all []string
+				for _, tasks := range t.spy.added {
+					all = append(all, tasks...)
 				}
+
+				Expect(t, all).To(Contain("task-0", "task-1", "task-2"))
+			})
+		})
+
+		o.Group("one task assigned", func() {
+			o.BeforeEach(func(t TO) TO {
+				t.spy.actual["worker-0"] = []string{"task-1"}
 				return t
 			})
 
-			o.Group("no tasks yet assigned", func() {
-				o.Spec("it evenly splits tasks among the cluster", func(t TO) {
-					t.o.NextTerm(context.TODO())
+			o.Spec("it does not replace the existing task", func(t TO) {
+				t.o.NextTerm(context.TODO())
 
-					Expect(t, t.spy.added).To(HaveLen(3))
+				Expect(t, t.spy.added).To(HaveLen(2))
 
-					Expect(t, t.spy.added["worker-0"]).To(HaveLen(1))
-					Expect(t, t.spy.added["worker-1"]).To(HaveLen(1))
-					Expect(t, t.spy.added["worker-2"]).To(HaveLen(1))
+				Expect(t, t.spy.added["worker-0"]).To(HaveLen(0))
+				Expect(t, t.spy.added["worker-1"]).To(HaveLen(1))
+				Expect(t, t.spy.added["worker-2"]).To(HaveLen(1))
 
-					var all []string
-					for _, tasks := range t.spy.added {
-						all = append(all, tasks...)
-					}
+				var all []string
+				for _, tasks := range t.spy.added {
+					all = append(all, tasks...)
+				}
 
-					Expect(t, all).To(Contain("task-0", "task-1", "task-2"))
-				})
+				Expect(t, all).To(Contain("task-0", "task-2"))
+			})
+		})
+
+		o.Group("extra task", func() {
+			o.BeforeEach(func(t TO) TO {
+				t.spy.actual["worker-0"] = []string{"extra"}
+				return t
 			})
 
-			o.Group("one task assigned", func() {
-				o.BeforeEach(func(t TO) TO {
-					t.spy.actual["worker-0"] = []string{"task-1"}
-					return t
-				})
+			o.Spec("it removes the task", func(t TO) {
+				t.o.NextTerm(context.TODO())
 
-				o.Spec("it does not replace the existing task", func(t TO) {
-					t.o.NextTerm(context.TODO())
+				Expect(t, t.spy.added).To(HaveLen(3))
+				Expect(t, t.spy.removed).To(HaveLen(1))
 
-					Expect(t, t.spy.added).To(HaveLen(2))
+				Expect(t, t.spy.removed["worker-0"]).To(HaveLen(1))
+				Expect(t, t.spy.removed["worker-1"]).To(HaveLen(0))
+				Expect(t, t.spy.removed["worker-2"]).To(HaveLen(0))
 
-					Expect(t, t.spy.added["worker-0"]).To(HaveLen(0))
-					Expect(t, t.spy.added["worker-1"]).To(HaveLen(1))
-					Expect(t, t.spy.added["worker-2"]).To(HaveLen(1))
+				Expect(t, t.spy.removed["worker-0"]).To(Contain("extra"))
+			})
+		})
 
-					var all []string
-					for _, tasks := range t.spy.added {
-						all = append(all, tasks...)
-					}
-
-					Expect(t, all).To(Contain("task-0", "task-2"))
-				})
+		o.Group("too many of a task", func() {
+			o.BeforeEach(func(t TO) TO {
+				t.spy.actual["worker-0"] = []string{"task-0"}
+				t.spy.actual["worker-1"] = []string{"task-0"}
+				return t
 			})
 
-			o.Group("extra task", func() {
-				o.BeforeEach(func(t TO) TO {
-					t.spy.actual["worker-0"] = []string{"extra"}
-					return t
-				})
+			o.Spec("it removes the task and adds the required", func(t TO) {
+				t.o.NextTerm(context.TODO())
 
-				o.Spec("it removes the task", func(t TO) {
-					t.o.NextTerm(context.TODO())
-
-					Expect(t, t.spy.added).To(HaveLen(3))
-					Expect(t, t.spy.removed).To(HaveLen(1))
-
-					Expect(t, t.spy.removed["worker-0"]).To(HaveLen(1))
-					Expect(t, t.spy.removed["worker-1"]).To(HaveLen(0))
-					Expect(t, t.spy.removed["worker-2"]).To(HaveLen(0))
-
-					Expect(t, t.spy.removed["worker-0"]).To(Contain("extra"))
-				})
-			})
-
-			o.Group("too many of a task", func() {
-				o.BeforeEach(func(t TO) TO {
-					t.spy.actual["worker-0"] = []string{"task-0"}
-					t.spy.actual["worker-1"] = []string{"task-0"}
-					return t
-				})
-
-				o.Spec("it removes the task and adds the required", func(t TO) {
-					t.o.NextTerm(context.TODO())
-
-					Expect(t, t.spy.removed).To(HaveLen(1))
-					Expect(t, t.spy.added).To(HaveLen(2))
-				})
+				Expect(t, t.spy.removed).To(HaveLen(1))
+				Expect(t, t.spy.added).To(HaveLen(2))
 			})
 		})
 	})
